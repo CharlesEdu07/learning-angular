ebf4f57c339733633ef88b83c28ec1143951063c
unknown <charleseduardofaria@gmail.com>
2024-03-19 12:01:07 -0300

    Binding com propriedades customizadas

---
+++ src/app/employee-card/employee-card.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-card',
+  templateUrl: './employee-card.component.html',
+  styleUrls: ['./employee-card.component.css']
+})
+export class EmployeeCardComponent {
+  @Input() employee: any;
+}
