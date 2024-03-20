bfe01f3c7c278ed5a71071e21ba2485651536486
unknown <charleseduardofaria@gmail.com>
2024-03-20 07:57:23 -0300

    Trabalhando com CSS no Angular

--- src/app/employee-card/employee-card.component.ts
+++ src/app/employee-card/employee-card.component.ts
@@ -3,7 +3,13 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-employee-card',
   templateUrl: './employee-card.component.html',
-  styleUrls: ['./employee-card.component.css']
+  //styleUrls: ['./employee-card.component.css']
+  styles: [`
+    .card-block {
+      text-transform: uppercase;
+      color: blue;
+    }
+  `]
 })
 export class EmployeeCardComponent {
   @Input() employee: any;
