dbd9d5ca7f26dca61918ef0c18af56ea9a41df98
unknown <charleseduardofaria@gmail.com>
2024-03-22 10:00:47 -0300

    Vinculando propriedades da tag com @HostBinding

--- src/app/colored-field.directive.ts
+++ src/app/colored-field.directive.ts
@@ -1,20 +1,27 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appColoredField]'
 })
 export class ColoredFieldDirective {
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
-  }
+  @HostBinding('style.backgroundColor')
+  backgroundColor: string;
+
+  // constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+  // }
 
   @HostListener('focus')
   onFocus() {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'yellow');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'yellow');
+
+    this.backgroundColor = 'green';
   }
 
   @HostListener('blur')
   onBlur() {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+
+    this.backgroundColor = 'transparent';
   }
 }
